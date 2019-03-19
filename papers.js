e92104e4d46418567b48c380d57ee6f2a7ce5af2
Ali Rostami <rostamiev@gmail.com>
2019-03-19 19:18:45 +0100

    Update papers.js
    small change

--- papers.js
+++ papers.js
@@ -1,7 +1,7 @@
 pubs = [
   {
     "authors": "M. Ali Rostami, Martin Bücker. ",
-    "title" : "An Inexact Combinatorial Model for Maximizing the Number of Discovered Nonzero Entries",
+    "title" : "An Inexact Combinatorial Model for Maximizing the Number of Discovered Nonzero Entries.",
     "proceedings" : "submitted for publication at IWOCA 2019",
     "year" : 2019,
     "keywords" : "CSC, GTA"
