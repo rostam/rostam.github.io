6bb143b0a053e199a97ea0d27f3b6c8a043efbc9
Ali Rostami <rostami@axxessio.com>
2022-05-12 20:37:14 +0200

    correcting scoring

--- persian_meetup/Quiz/script.js
+++ persian_meetup/Quiz/script.js
@@ -5,6 +5,8 @@
 var countQues=0;
 var lang;
 var score=0;
+var correct = 0;
+var incorrect = 0;
 
 document.getElementById("score").textContent="Score : "+0;
 document.querySelector(".view-results").style.display="none";
@@ -12,9 +14,9 @@ document.querySelector(".quiz").style.display="none";
 document.querySelector(".final-result").style.display="none";
 
 
-if(localStorage.getItem("countQues") != null){
-    countQues = parseInt(localStorage.getItem("countQues"));
-}
+// if(localStorage.getItem("countQues") != null){
+//     countQues = parseInt(localStorage.getItem("countQues"));
+// }
 document.querySelector(".choose-lang").addEventListener("click",function(){
 
     lang=document.getElementById("language").value+"Questions";
@@ -49,18 +51,20 @@ document.querySelector(".submit-answer").addEventListener("click",function(){
            
         score+=10;
         document.getElementById("score").textContent="Score : "+score;
-        document.getElementById("ques-view").innerHTML+="<div class='ques-circle correct'>"+(countQues+1)+"</div>";
+        correct++;
+        // document.getElementById("ques-view").innerHTML+="<div class='ques-circle correct'>"+(countQues+1)+"</div>";
            
     }else{
     
         score-=5;
         document.getElementById("score").textContent="Score : "+score;
-        document.getElementById("ques-view").innerHTML+="<div class='ques-circle incorrect'>"+(countQues+1)+"</div>";
+        incorrect++;
+        // document.getElementById("ques-view").innerHTML+="<div class='ques-circle incorrect'>"+(countQues+1)+"</div>";
     };
     
     if (countQues<window[lang].length-1){
         countQues++;
-        localStorage.setItem("countQues",countQues);
+        // localStorage.setItem("countQues",countQues);
     }else{
         document.querySelector(".submit-answer").style.display="none";
         // document.querySelector(".view-results").style.display="unset";
@@ -90,11 +94,13 @@ document.querySelector(".view-results").addEventListener("click",function(){
     countQues
     document.querySelector(".final-result").style.display="block";
     
-    document.querySelector(".solved-ques-no").innerHTML="You Solved "+(countQues+1)+" questions of "+document.getElementById("language").value;
+    all = correct + incorrect;
+    document.querySelector(".solved-ques-no").innerHTML="You Solved " + all
+        + " questions of "+document.getElementById("language").value;
     
-    var correct= document.getElementById("ques-view").getElementsByClassName("correct").length;
+    // var correct= document.getElementById("ques-view").getElementsByClassName("correct").length;
     
-    document.querySelector(".right-wrong").innerHTML=correct+" were Right and "+((countQues+1)-correct)+" were Wrong";
+    document.querySelector(".right-wrong").innerHTML=correct+" were Right and "+incorrect+" were Wrong";
     
     document.getElementById("display-final-score").innerHTML="Your Final Score is: "+score;
     
@@ -103,7 +109,7 @@ document.querySelector(".view-results").addEventListener("click",function(){
     }else if(correct/(countQues+1)>0.6){
         document.querySelector(".remark").innerHTML="Remark: Good, Keep Improving.";
     
-    }else if(correct/(countQues+1)){
+    }else if(correct/incorrect > 0.4) {
         document.querySelector(".remark").innerHTML="Remark: Satisfactory, Learn More.";
     }else{
         document.querySelector(".remark").innerHTML="Remark: Unsatisfactory, Try Again.";
